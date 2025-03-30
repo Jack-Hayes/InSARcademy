afef7c8153228ebb980e9b5879da9ccd63c1e1ee
Jack-Hayes <jehayes@uw.edu>
2025-03-29 21:14:59 -0700

    Fix publish_dir path in GitHub Actions workflow

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'InSARcademy Website'
+copyright = '2025, Jack Hayes'
+author = 'Jack Hayes'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "html_admonition",
+    "html_image",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,16 @@
+# Welcome to InSARcademy
+
+This is a digestible website for the InSARcademy repository.
+
+- [Introduction](introduction.md)
+- [Tutorials](tutorials.md)
+- [Resources](resources.md)
+
+```{toctree}
+:maxdepth: 2
+:caption: Contents:
+
+introduction
+tutorials
+resources
+```
